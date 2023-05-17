56f928da623aed874ccdb030cf77f3a4349849ed
Usov Dmytro <dioks.2013@gmail.com>
2023-05-17 21:32:17 +0300

    2

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import random
 
 
+def update_letters(chosen_word, encrypted_word, attempt):
+    new_encrypted_word = ''
+    for i in range(len(chosen_word)):
+        new_encrypted_word += chosen_word[i] if chosen_word[i] == attempt else encrypted_word[i]
+    return new_encrypted_word
+
+
 def main():
     word_list = ['повітря', 'швидкість', 'вікно', 'спроба', 'книга', 'шухлядка']
     chosen_word = random.choice(word_list)
     encrypted_word = '*'*len(chosen_word)
-    quantity_selected = False
     number_of_attempts = 0
-    while not quantity_selected:
+    while True:
         number_of_attempts = input('Скільки буде спроб?: ')
         if number_of_attempts.isdecimal() and int(number_of_attempts) > 0:
-            quantity_selected = True
             number_of_attempts = int(number_of_attempts)
+            break
         else:
             print('Треба вказати ціле число більше 0')
-    while number_of_attempts:
+    for i in range(number_of_attempts):
         attempt = input('Вкажи букву чи слово: ').lower()
         if len(attempt) == 1:
             letter_count = chosen_word.count(attempt)
             if letter_count:
-                new_encrypted_word = ''
-                for i in range(0, len(chosen_word)):
-                    new_encrypted_word += chosen_word[i] if chosen_word[i] == attempt else encrypted_word[i]
-                encrypted_word = new_encrypted_word
+                encrypted_word = update_letters(chosen_word, encrypted_word, attempt)
                 print('Вгадав!')
                 if encrypted_word == chosen_word:
                     print('Ви відгадали слово!')
@@ -36,12 +39,9 @@ def main():
                 return
             else:
                 print('Не вгадав!')
-
         print(encrypted_word)
-        number_of_attempts -= 1
     print('Кількість спроб закінчилися!')
+
 
 if __name__ == '__main__':
     main()
-
-
